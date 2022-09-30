ccaf11c757280d1184c590861cd36dd3c2affe8e
nami <almost.nami@gmail.com>
2022-09-30 13:41:46 +0900

    commit 3

--- src/test/java/org/zerock/service/SampleTxServiceTests.java
+++ src/test/java/org/zerock/service/SampleTxServiceTests.java
@@ -16,6 +16,12 @@ public class SampleTxServiceTests {
     @Setter(onMethod_ = {@Autowired})
     private SampleTxService service;
 
+    /*
+        테스트에 사용한 문자열 str은 82bytes
+        -> @Transactional이 없을 때는 tbl_sample1에는 데이터가 추가되지만,
+            tbl_sample2에는 길이 제한으로 실패하게 됨
+        -> @Transactional을 추가한 뒤에는 rollback() 처리 됨
+     */
     @Test
     public void testLong() {
         String str = "Starry\r\n" +
